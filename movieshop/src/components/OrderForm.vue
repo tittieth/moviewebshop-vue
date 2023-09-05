4b54d8b9d5723edfedafb23dd1338a2d8f86ffb6
EZillman <evelina.zillman@medieinstitutet.se>
2023-09-05 16:37:36 +0200

    [Added] html form

---
+++ movieshop/src/components/OrderForm.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <form>
+        <label>
+            <span>Förnamn</span>
+            <input type="text" />
+        </label>
+        <label>
+            <span>Efternamn</span>
+            <input type="text" />
+        </label>
+        <label>
+            <span>Mailadress</span>
+            <input type="email" />
+        </label>        
+        <label>
+            <span>Telefonnummer</span>
+            <input type="tel" />
+        </label>      
+        <span>Betalsätt</span> <!-- Lägg till v-if om kort är vald, input för kortnummer-->
+        <label>
+            <span>Swish</span>
+            <input type="radio" name="payment_method" />
+        </label>
+        <label>
+            <span>Kort</span>
+            <input type="radio" name="payment_method"/>
+        </label>
+    </form>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
