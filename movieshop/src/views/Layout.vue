7bfa4113b2463c7991e1923f6386bdfd8f8e7194
EZillman <evelina.zillman@medieinstitutet.se>
2023-09-07 09:22:02 +0200

    [fixed] input styling to main

--- movieshop/src/views/Layout.vue
+++ movieshop/src/views/Layout.vue
@@ -2,13 +2,15 @@
     <div>
         <NavView></NavView>
       <main>
-        <router-view></router-view>
+        <router-view>
+          <SearchMovies></SearchMovies>
+        </router-view>
       </main>
     </div>
   </template>
   
   <script setup lang="ts">
-    import OrderForm from "@/components/OrderForm.vue";
+import SearchMovies from "@/components/SearchMovies.vue";
 import NavView from "./NavView.vue";
   </script>
   
