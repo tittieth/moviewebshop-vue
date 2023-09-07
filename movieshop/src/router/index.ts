e742aa4019ef9e45f475639040fa9f12bbd7bbf0
tittieth <114925706+tittieth@users.noreply.github.com>
2023-09-07 11:18:39 +0200

    [Added] routerlinks

--- movieshop/src/router/index.ts
+++ movieshop/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '../views/Layout.vue'
 import HomeView from '../views/HomeView.vue'
 import MovieView from '../views/MovieView.vue'
+import CartView from '../views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
           name: "movie",
           component: MovieView,
           props: true,
+        },
+        {
+          path: '/cart',
+          name: 'cartview',
+          component: CartView,
         }
       ]
     },
--- movieshop/src/views/CartView.vue
+++ movieshop/src/views/CartView.vue
@@ -1,8 +1,14 @@
-
 <script setup lang="ts">
     import OrderForm from "@/components/OrderForm.vue";
+    import { cart } from "@/helpers/cart";
+
+    const cartItems = cart.value
+    console.log('cartview' + cartItems);
+    
 </script>
   
 <template>
+    <div v-for="item in cart" :key="item.productId">{{ item.product }}</div>
+    <h1>Hello from cartview</h1>
     <OrderForm></OrderForm>
 </template>
