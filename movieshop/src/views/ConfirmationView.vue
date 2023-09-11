964d38e211b3cadab8bf4dfe85e8b903f4f041bf
tittieth <114925706+tittieth@users.noreply.github.com>
2023-09-11 15:17:30 +0200

    [Improved] styling

--- movieshop/src/views/ConfirmationView.vue
+++ movieshop/src/views/ConfirmationView.vue
@@ -15,11 +15,19 @@ console.log(orderName)
 
 <style scoped lang="scss">
 .wrapper {
+  width: 90%;
+  margin: auto;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 50px;
   background-color: #9d0b28;
   padding: 20px;
+
+  h1 {
+    font-size: 1.4rem;
+    padding: 5px;
+  }
 }
 </style>
