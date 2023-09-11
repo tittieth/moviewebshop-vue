130725e073e7d837817153bdd6c616423873b8ed
Melvin Kempe <k3mp314@gmail.com>
2023-09-11 08:30:14 +0200

    [Moved] &:hover attriute in the css so that it applies to buttons. [Removed] the orderForm tag from the layout

--- movieshop/src/views/Layout.vue
+++ movieshop/src/views/Layout.vue
@@ -2,7 +2,7 @@
     <div>
         <NavView></NavView>
       <main>
-        <router-view><OrderForm></OrderForm></router-view>
+        <router-view></router-view>
       </main>
     </div>
   </template>
