5936d7f2b2f088542b8c5f4657f895d2f6c60c95
tittieth <114925706+tittieth@users.noreply.github.com>
2023-09-12 11:59:27 +0200

    [Added] styling for link and img

--- movieshop/src/components/NotFound.vue
+++ movieshop/src/components/NotFound.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts"></script>
+
 <template>
-     <div className="not-found">
-            <h1>The page you are looking for may have been moved, deleted or possibly never existed</h1>
-            <span className="error-number">404</span>
-        </div>
+  <div class="not-found">
+    <div>
+      <img src="../assets/images/kopiera.jpg" height="200" width="200" />
+    </div>
+    <router-link to="/" class="home-link">Hem</router-link>
+  </div>
 </template>
+
 <style lang="scss">
+.not-found {
+  width: 90%;
+  margin: auto;
+  margin-top: 10%;
+  font-size: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    width: 100%;
+    height: 100%;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .home-link {
+    font-size: 1.5rem;
+    background-color: #baaecf;
+    border-radius: 60px;
+    box-shadow: 0 3px 3px 0 #00000073;
+    padding: 5px 15px;
+    margin-top: 15px;
+  }
+}
 </style>
