0aa0ea968d876468d51ca7edd93dfb764b10f3c1
tittieth <114925706+tittieth@users.noreply.github.com>
2023-09-12 09:45:38 +0200

    [Added] notfound component

---
+++ movieshop/src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+     <div className="not-found">
+            <h1>The page you are looking for may have been moved, deleted or possibly never existed</h1>
+            <span className="error-number">404</span>
+        </div>
+</template>
+<style lang="scss">
+</style>
